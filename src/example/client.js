0613c79bce332e059f4577649de1962c4f6bdb12
Saurabh Gupta <saurabhg22596@gmail.com>
2021-02-15 15:20:15 +0530

    Refactor

--- src/example/client.js
+++ src/example/client.js
@@ -2,12 +2,13 @@
 const socket = io('http://localhost:3000');
 
 socket.on('connect', () => {
-    console.log("connected")
+    console.log("connected");
+    socket.emit('createEvent', { name: 'clientEvent', data: { email: "test@email.com" } }, (resp) => {
+        console.log("createEvent:clientEvent", resp) // TODO: This is not working
+    });
 });
 
-socket.emit('createEvent', { name: 'clientEvent', data: { email: "test@email.com" } }, (resp) => {
-    console.log("createEvent:clientEvent", resp) // TODO: This is not working
-});
+
 
 socket.on('disconnect', () => {
     console.log("disconnected")
